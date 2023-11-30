0a7ece95ea375d15b762415cd0d868c0f1f9d497
Christopher Kenny <ctk44@cornell.edu>
2023-11-30 17:50:41 -0500

    check fixes

--- R/by_plan.R
+++ R/by_plan.R
@@ -16,7 +16,7 @@
 #'
 by_plan <- function(x, ndists) {
   if (missing(ndists)) {
-    reps <- unique(rle(c(na.omit(x)))$lengths)
+    reps <- unique(rle(c(stats::na.omit(x)))$lengths)
     ndists <- ifelse(length(reps) == 1, reps, Reduce(gcd, reps))
   }
 
